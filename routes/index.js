faf518a4b27177575b6602925023023aa0bad378
Jeff <lanfeng.sun@yahoo.ca>
2018-02-15 01:48:42 +0000

    update sign up message

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.post("/register", function(req, res) {
             return res.render("register", {error: err.message});
         }
         passport.authenticate("local")(req, res, function() {
-            req.flash("success", "Successfully Signed Up. Welcome to YelpCamp, " + user);
+            req.flash("success", "Successfully Signed Up. Welcome to YelpCamp, " + user.username);
             res.redirect("/campgrounds");
         });
     });
